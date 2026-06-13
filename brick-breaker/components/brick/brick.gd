01b2cd30ea02eeb8a339c44324b7f38b40bed104
Gaetan Rizio <gaetan@rizio.fr>
2026-06-13 18:45:17 +0200

    bricks have life

--- brick-breaker/components/brick/brick.gd
+++ brick-breaker/components/brick/brick.gd
@@ -5,11 +5,21 @@ class_name Brick extends Node2D
         color = value
         if is_node_ready():
             polygon.color = color
+@export var life: int = 1
 
 @onready var polygon: Polygon2D = $Polygon2D
+@onready var life_label: Label = $LifeLabel
+
+func _ready() -> void:
+    polygon.color = color
+    life_label.text = str(life)
 
 func hit() -> void:
-    queue_free()
+    life -= 1
+    if life <= 0:
+        queue_free()
+    else:
+        life_label.text = str(life)
 
 func on_ball_collision(_ball: Ball) -> void:
     hit()
