55b66d627a951bd465d81eaeceedff5db609db8d
Gaetan Rizio <gaetan@rizio.fr>
2026-06-25 20:08:07 +0200

    better water rendering

--- boat/water.gd
+++ boat/water.gd
@@ -1,8 +1,8 @@
 class_name Water extends MeshInstance3D
 
-const speed: Vector3 = Vector3(0.1, 0.1, 1)
+const speed: Vector3 = Vector3(0.1, 0.1, 2)
 
-@onready var noise: FastNoiseLite = get_surface_override_material(0).roughness_texture.noise
+@onready var noise: FastNoiseLite = get_surface_override_material(0).normal_texture.noise
 
 func _process(delta: float) -> void:
     noise.offset = noise.offset + speed * delta
