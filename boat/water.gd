1162cc108921c1d579cafa551edddc5a4a58c6b9
Gaetan Rizio <gaetan@rizio.fr>
2026-06-26 07:20:51 +0200

    better water effect

--- boat/water.gd
+++ boat/water.gd
@@ -1,8 +1,11 @@
 class_name Water extends MeshInstance3D
 
-const speed: Vector3 = Vector3(0.1, 0.1, 2)
+const normal_speed: Vector3 = Vector3(1, 1, 2)
+const bent_speed: Vector3 = Vector3(-1, -1, 2)
 
-@onready var noise: FastNoiseLite = get_surface_override_material(0).normal_texture.noise
+@onready var normal_noise: FastNoiseLite = get_surface_override_material(0).normal_texture.noise
+@onready var bent_noise: FastNoiseLite = get_surface_override_material(0).bent_normal_texture.noise
 
 func _process(delta: float) -> void:
-    noise.offset = noise.offset + speed * delta
+    normal_noise.offset = normal_noise.offset + normal_speed * delta
+    bent_noise.offset = bent_noise.offset + bent_speed * delta
